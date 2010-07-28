a07e61b0c90dde141abd01f55ecb4c61266626e8
Chris Bilson <cbilson@pobox.com>
2010-07-28 13:03:52 -0700

    started to add really complicated example

---
+++ QueryExamples/ReallyReallyComplicatedExample/Account.cs
@@ -0,0 +1,41 @@
+﻿using FluentMigrator;
+using FluentNHibernate.Mapping;
+
+namespace QueryExamples.ReallyReallyComplicatedExample
+{
+    public class Account
+    {
+        public virtual int ID { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string AccountNumber { get; set; }
+    }
+
+    public class AccountMap : ClassMap<Account>
+    {
+        public AccountMap()
+        {
+            Id(x => x.ID).GeneratedBy.Identity();
+            Map(x => x.Name);
+            Map(x => x.AccountNumber);
+        }
+    }
+
+    [Migration(2)]
+    public class CreateAccountTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Account")
+                .WithColumn("ID").AsInt32().PrimaryKey().Identity().NotNullable()
+                .WithColumn("Name").AsString(50).NotNullable()
+                .WithColumn("AccountNumber").AsString(50).NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Account");
+        }
+    }
+
+
+}
