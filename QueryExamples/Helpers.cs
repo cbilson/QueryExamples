86a79a34bbe738259023ea39697ecda6a9879b45
Chris Bilson <cbilson@pobox.com>
2010-08-24 07:48:51 -0700

    got everything working with the new fluent migrator

--- QueryExamples/Helpers.cs
+++ QueryExamples/Helpers.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using FluentMigrator;
 using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
 using FluentMigrator.Runner.Processors;
 using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
@@ -19,16 +20,31 @@ namespace QueryExamples {
 
             Console.WriteLine("Updating database...");
 
-            var processorFactory = ProcessorFactory.GetFactory("Sqlite");
-            var processor = processorFactory.Create("Data Source=Data.db");
-
-            var conventions = new MigrationConventions();
-            var runner = new MigrationVersionRunner(conventions, processor,
-                                                    new MigrationLoader(conventions), 
-                                                    typeof(Helpers).Assembly,
-                                                    migrationsNamespace);
+            var runner = GetRunner(migrationsNamespace);
 
             runner.MigrateUp();
+        }
+
+        private static MigrationVersionRunner GetRunner(string migrationsNamespace)
+        {
+            var conventions = new MigrationConventions();
+
+            var announcer = new BaseAnnouncer(Console.WriteLine);
+
+            var processor = GetProcessor(announcer);
+
+            return new MigrationVersionRunner(conventions, processor,
+                                              new MigrationLoader(conventions), 
+                                              typeof(Helpers).Assembly,
+                                              migrationsNamespace, announcer, "");
+        }
+
+        private static IMigrationProcessor GetProcessor(BaseAnnouncer announcer)
+        {
+            var processorFactory = ProcessorFactory.GetFactory("Sqlite");
+
+            var options = new ProcessorOptions { };
+            return processorFactory.Create("Data Source=Data.db", announcer, options);
         }
 
         public static ISession OpenSession(Action<MappingConfiguration> mappings) {
